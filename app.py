38bff9fa945bd764e2a99222fb09c1b4c4f44508
Settasit7 <108485042+Settasit7@users.noreply.github.com>
2022-07-09 16:30:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title = 'Stocker', page_icon = ':chart_with_upwards_trend:' , layout = 'wide')
+st.set_page_config(page_title = 'Stocker', page_icon = ':space_invader:' , layout = 'wide')
 
 hide_st_style = '''
 <style>
@@ -14,10 +14,10 @@ st.markdown(hide_st_style, unsafe_allow_html = True)
 
 @st.experimental_memo(suppress_st_warning=True)
 def function():
-    
+
     st.title('Stocker')
     st.write('##')
-    st.write('เว็บแอปทำนายราคาปิดหุ้นในอนาคตด้วยปัญญาประดิษฐ์ :space_invader:')
+    st.write('เว็บแอปทำนายราคาปิดหุ้นในอนาคตด้วยปัญญาประดิษฐ์')
 
     st.write('---')
 
@@ -41,150 +41,80 @@ def function():
         st.write('##')
         date1 = st.text_input('ดึงข้อมูลมาตั้งแต่วันที่เท่าไหร่', '2022-01-01')
         st.write('##')
-        date2 = st.text_input('ดึงข้อมูลมาจนถึงวันที่เท่าไหร่', '2022-06-30')
-        st.write('##')
         st.write('หมายเหตุ:')
-        st.write('วันที่เริ่มต้นการดึงข้อมูลต้องอยู่ก่อนวันที่สิ้นสุดการดึงข้อมูล')
+        st.write('โปรแกรมจะดึงข้อมูลมาตั้งแต่วันที่ในช่องด้านบนจนถึงเมื่อวาน')
 
     with column3:
         st.write('##')
         st.write('3. ทำนายอนาคต')
         st.write('##')
-        date3 = st.text_input('ทำนายราคาปิดจนถึงวันที่เท่าไหร่', '2022-08-31')
-        st.write('##')
-        day = st.number_input('ทำนายโดยใช้ข้อมูลย้อนหลังทั้งหมดกี่วัน', min_value = 1, value = 60)
-        st.write('##')
-        percent = st.number_input('แบ่งข้อมูลที่ดึงมาใช้ในการเทรนกี่เปอร์เซ็นต์', min_value = 1, max_value = 100, value = 90)
+        date3 = st.text_input('ทำนายราคาปิดจนถึงวันที่เท่าไหร่', '2022-09-30')
         st.write('##')
         st.write('หมายเหตุ:')
-        st.write('จำนวนข้อมูลที่จะใช้ทำนายต้องน้อยกว่าหรือเท่ากับจำนวนข้อมูลที่ดึงมาจากขั้นตอนก่อนหน้า')
-        st.write('ปรับเปอร์เซ็นต์ข้อมูลที่แบ่งมาใช้ในการเทรนเป็น 100 หากต้องการทำนายอนาคตจากข้อมูลที่เป็นปัจจุบันมากที่สุด ในกรณีนี้จะไม่แสดงกราฟช่วงเทส และแสดงค่า rmse เป็น 0')
+        st.write('โปรแกรมจะทำนายราคาปิดตั้งแต่วันแรกที่ดึงข้อมูลจนถึงวันที่ในช่องด้านบน')
 
     st.write('---')
 
     with st.spinner('แปป...'):
 
-        import math
-        import numpy as np
-        import pandas as pd
+        from datetime import date
+
+        date2 = date.today().strftime('%Y-%m-%d')
+
         import yfinance as yf
-        import matplotlib.pyplot as plt
-        from keras.models import Sequential
-        from keras.layers import Dense, LSTM
-        from sklearn.preprocessing import MinMaxScaler
-        plt.style.use('https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-light.mplstyle')
-
-        ratio = percent / 100
-
-        input = day
-        dense = math.ceil(((2 / 3) * input) + 1)
-        output = 1
-
-        optimizer = 'nadam'
-        loss = 'mean_squared_error'
 
         df0 = yf.download(name, date1, date2)
-        df1 = df0.filter(['Close'])
-        le0 = len(df1)
+        df0.reset_index(inplace = True)
 
-        date_list = [i.strftime('%Y-%m-%d 00:00:00') for i in pd.date_range(date2, date3, freq = 'B')]
-        df2 = pd.DataFrame(date_list)
-        df2.rename(columns = {df2.columns[0]: 'Date'}, inplace = True)
-        df2['Predictions'] = np.nan
-        df2.set_index('Date', inplace = True)
-        df2.index = pd.to_datetime(df2.index)
+        df1 = df0[['Date', 'Close']]
+        df1 = df1.rename(columns = {'Date': 'ds', 'Close': 'y'})
+
+        from fbprophet import Prophet
+
+        m = Prophet()
+        m.fit(df1)
+
+        import pandas as pd
+
+        df2 = [i.strftime('%Y-%m-%d 00:00:00') for i in pd.date_range(date2, date3, freq = 'B')]
+        df2 = pd.DataFrame(df2)
+        df2.rename(columns = {df2.columns[0]: 'ds'}, inplace = True)
 
         df3 = pd.concat([df1, df2])
-        df3 = df3.fillna(np.nan)
-        df4 = df3.filter(['Close'])
-        ra1 = df4.values
-        le1 = math.ceil(le0 * ratio)
-        df3.reset_index(inplace = True)
 
-        scaler = MinMaxScaler(feature_range = (0,1))
-        sc1 = scaler.fit_transform(ra1)
+        df4 = m.predict(df3)
 
-        tr0 = sc1[0:le1,:]
-        tr1 = []
-        tr2 = []
-        for i in range(day, le1):
-            tr1.append(tr0[i-day:i,0])
-            tr2.append(tr0[i,0])
+        df4 = df4.rename(columns = {'ds': 'Date', 'yhat': 'Predict^', 'yhat_lower': 'Predict^Lower', 'yhat_upper': 'Predict^Upper'})
+        df4 = df4[['Date', 'Predict^', 'Predict^Lower', 'Predict^Upper']]
+        df4.set_index('Date',inplace = True)
+        df4.index = pd.to_datetime(df4.index)
 
-        tr1, tr2 = np.array(tr1), np.array(tr2)
-        tr1 = np.reshape(tr1, (tr1.shape[0], tr1.shape[1], 1))
+        df5 = df4[len(df0):]
 
-        model = Sequential()
-        model.add(LSTM(input, return_sequences = True, input_shape = (tr1.shape[1], 1)))
-        model.add(LSTM(input, return_sequences = False))
-        model.add(Dense(dense))
-        model.add(Dense(output))
-        model.compile(optimizer, loss)
-        model.fit(tr1, tr2, batch_size = 1, epochs = 3)
+        df0 = df0[['Date', 'Open', 'High', 'Low', 'Close']]
+        df0.set_index('Date',inplace = True)
+        df0.index = pd.to_datetime(df0.index)
 
-        te0 = sc1[le1 - day:,:]
-        le2 = len(te0)
-        le = len(sc1)
-        te1 = []
-        te1.append(sc1[le1 - day:le1,0])
-        te1 = np.array(te1)
-        te1 = np.reshape(te1, (te1.shape[0], te1.shape[1], 1))
+        from plotly import graph_objs as go
+        from plotly.subplots import make_subplots
 
-        for i in range(le1 + 1, le):
-            re = model.predict(te1)
-            ta = scaler.inverse_transform(re)
-            ta = ta[0].tolist()[0]
-            df3.loc[i, 'Predictions'] = ta
-            re = re.tolist().pop(0).pop(0)
-            te1 = te1.tolist().pop(0)
-            te3 = []
-            for j in range(day):
-                te3.append(te1.pop(0).pop(0))
-            te3.pop(0)
-            te3.append(re)
-            te4 = []
-            te4.append(te3[:])
-            te1 = te4
-            te1 = np.array(te1)
-            te1 = np.reshape(te1, (te1.shape[0], te1.shape[1], 1))
+        fig = make_subplots(specs = [[{"secondary_y": True}]])
+        fig.update_layout(autosize = False, width = 1000, height = 600)
+        fig.add_trace(go.Scatter(x = df4.index, y = df4['Predict^'], name = 'Predict^', marker_color = 'blue'))
+        fig.add_trace(go.Scatter(x = df4.index, y = df4['Predict^Upper'], name = 'Predict^Upper', marker_color = 'lightblue', showlegend = False))
+        fig.add_trace(go.Scatter(x = df4.index, y = df4['Predict^Lower'], name = 'Predict^Lower', marker_color = 'lightblue', showlegend = False))
+        fig.add_trace(go.Candlestick(x = df0.index, open = df0['Open'], high = df0['High'], low = df0['Low'], close = df0['Close'], name = 'Real Data'))
+        df0.loc[df0['Close'] - df0['Open'] >= 0, 'color'] = 'green'
+        df0.loc[df0['Close'] - df0['Open'] <   0, 'color'] = 'red'
+        fig.update_xaxes(rangebreaks = [dict(bounds = ['sat', 'mon'])])
+        fig.update_layout(title={'text': name, 'x': 0.5})
 
-        te2 = ra1[le1:le0,:]
+        import math
+        import numpy as np
 
-        df5 = df3
-
-        df3.set_index('Date',inplace = True)
-        df3.index = pd.to_datetime(df3.index)
-
-        df6 = df5.iloc[le1 + 1:,:]
-
-        da1 = df3.filter(['Close'])
-        da2 = df3.filter(['Predictions'])
-        li1 = da1[:le1]
-        li2 = da1[le1:le0]
-        li3 = da2[le1:le0]
-        li4 = da2[le0:]
-
-        rmse1 = 0
-        for i in range(le1 + 1, le0):
-            pop1 = df5.filter(['Close']).values.tolist()[i].pop(0)
-            pop2 = df5.filter(['Predictions']).values.tolist()[i].pop(0)
-            rmse0 = (pop1 - pop2) ** 2
-            rmse1 = rmse1 + rmse0
-        if le0 - le1 - 1 == 0:
-            rmse = 'undefined'
-        else:
-            rmse2 = rmse1 / (le0 - le1 - 1)
-            rmse = rmse2 ** 0.5
-
-        fig = plt.figure(figsize = (20,8))
-        plt.title(name, fontsize = 18)
-        plt.xlabel('Date', fontsize = 18)
-        plt.ylabel('Close Price THB', fontsize = 18)
-        plt.plot(li1['Close'], c = 'violet')
-        plt.plot(li2['Close'], c = 'darkviolet')
-        plt.plot(li3['Predictions'], c = 'green')
-        plt.plot(li4['Predictions'], c = 'limegreen')
-        plt.legend(['Real Data (Training)', 'Real Data (Testing)', 'Prediction (Testing)', 'Prediction (Future)'], loc = 'lower left')
+        y1 = df0.values[: , 0]
+        y2 = df4.values[: len(df0), 0]
+        RMSE = math.sqrt(np.square(np.subtract(y1, y2)).mean())
 
     st.success('เสร็จละ')
 
@@ -193,10 +123,10 @@ def function():
 
     st.write(fig)
 
-    st.write(df6)
+    st.write(df5)
 
-    st.write('Test Root Mean Square Error: ')
-    st.write(rmse)
+    st.write('Root Mean Square Error: ')
+    st.write(RMSE)
 
     st.write('---')
 
@@ -209,17 +139,21 @@ def function():
     st.header('มาคุยกัน')
     st.write('##')
 
-    contact_form = '''
-    <form action = 'https://formsubmit.co/44cb3e677835b6d430f0e09576b4dece' method = 'POST'>
-        <input type = 'text' name = 'name' placeholder = 'ชื่อ' required>
-        <input type = 'email' name = 'email' placeholder = 'อีเมล' required>
-        <textarea name = 'message' placeholder = 'ข้อความ' required></textarea>
-        <button type="submit">ส่ง</button>
-    </form>
-    '''
+    col1, col2 = st.columns(2)
 
-    st.markdown(contact_form, unsafe_allow_html = True)
-    
+    with col1:
+
+        contact_form = '''
+        <form action = 'https://formsubmit.co/44cb3e677835b6d430f0e09576b4dece' method = 'POST'>
+            <input type = 'text' name = 'name' placeholder = 'ชื่อ' required>
+            <input type = 'email' name = 'email' placeholder = 'อีเมล' required>
+            <textarea name = 'message' placeholder = 'ข้อความ' required></textarea>
+            <button type="submit">ส่ง</button>
+        </form>
+        '''
+
+        st.markdown(contact_form, unsafe_allow_html = True)
+
 function()
 
 st.experimental_memo.clear()
